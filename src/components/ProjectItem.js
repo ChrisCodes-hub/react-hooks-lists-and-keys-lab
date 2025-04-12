a064a198dade6a8d00b259ddf14e2039feeaec2d
christopherchege <chrisshege35@gmail.com>
2025-04-12 03:29:26 +0300

    completed lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,10 +6,14 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech) => (
+          <span key={tech}>{tech}</span>
+        ))}{/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
+  
